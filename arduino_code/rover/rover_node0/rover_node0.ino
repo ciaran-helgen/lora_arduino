71091e8afb3f584b0dbf3e5a7acf2912b9e4c194
ciaran13 <ciaran@helgen.tech>
2022-05-16 16:49:48 +0100

    new file:   arduino_code/basestation/base_node0/base_node0.ino 	new file:   arduino_code/rover/rover_node0/rover_node0.ino

---
+++ arduino_code/rover/rover_node0/rover_node0.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  while (!LoRa.begin(868E6)) {
+    Serial.println("Starting LoRa failed!");
+    Serial.println("Retrying...");
+    delay(500);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(50);
+}
